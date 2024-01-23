14999865f7eeb7132380bbf40453c682ab971f43
Alex T <ksanderer@gmail.com>
2024-01-23 18:19:27 +0000

    Create 1_Test_sub_page_1.py

---
+++ pages/1_Test_sub_page_1.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import pydeck as pdk
+from urllib.error import URLError
+
+st.set_page_config(page_title="Mapping Demo", page_icon="🌍")
+
+st.markdown("# Mapping Demo")
+st.sidebar.header("Mapping Demo")
+st.write(
+    """This demo shows how to use"""
+)
